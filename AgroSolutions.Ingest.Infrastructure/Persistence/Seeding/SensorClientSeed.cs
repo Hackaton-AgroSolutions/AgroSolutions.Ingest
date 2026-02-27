940339b6807f562f4ca9c4aba89ede71604ade0b
MarioGuilherme <marioguilhermedev@gmail.com>
2026-02-27 10:05:01 -0300

    feat: ssed

--- AgroSolutions.Ingest.Infrastructure/Persistence/Seeding/SensorClientSeed.cs
+++ AgroSolutions.Ingest.Infrastructure/Persistence/Seeding/SensorClientSeed.cs
@@ -14,11 +14,11 @@ public static class SensorClientSeed
             {
                 new(Guid.NewGuid(), "sensor-001", "$2a$12$4AEaYq3LAX6AExKUruehWOCFfr0jfPgDt/xr6lqwnnNM4t84JiOTS", 1), // Password: passSensor001
                 new(Guid.NewGuid(), "sensor-002", "$2a$12$4dXdDPoJRyyzuEx/ZRiiieIAR0Mei2Vfvmi8YyOZK8PefQBpXkYn2", 2), // Password: passSensor002
-                new(Guid.NewGuid(), "sensor-003", "$2a$12$7OtRv/kZ5o0IvpLvd0R8mu5f5C5kfcA.GDYRZ9lRhqqSRg61nk5bO", 3), // Password: passSensor003
-                new(Guid.NewGuid(), "sensor-004", "$2a$12$4dXdDPoJRyyzuEx/ZRiiieIAR0Mei2Vfvmi8YyOZK8PefQBpXkYn2", 4), // Password: passSensor004
-                new(Guid.NewGuid(), "sensor-005", "$2a$12$7OtRv/kZ5o0IvpLvd0R8mu5f5C5kfcA.GDYRZ9lRhqqSRg61nk5bO", 5), // Password: passSensor005
-                new(Guid.NewGuid(), "sensor-006", "$2a$12$mTw85pFcfdq3razNgwq7meErx2vtDnUrD.fc20QbrbcPy2tY35yri", 6), // Password: passSensor006
-                new(Guid.NewGuid(), "sensor-007", "$2a$12$tI28NigfYGN4Q17PyZmFsezo2r4SHvksnZcE6r6wZzjMf1Tlcj7.O", 7), // Password: passSensor007
+                new(Guid.NewGuid(), "sensor-003", "$2a$12$njBb8bEPH./jrDxZykergOP20FXK61ZvtpQ098P2Dsm.TXr6EzAUa", 3), // Password: passSensor003
+                new(Guid.NewGuid(), "sensor-004", "$2a$12$J0VLzaL6GReAIwitTAS1BeXfcHbL6Dkpu5UPl0Ie9U8PEDqiAJyEK", 4), // Password: passSensor004
+                new(Guid.NewGuid(), "sensor-005", "$2a$12$Bn886ucTafNJ91e98/DxyOn0p1pXbfxUXyInMSCcGw3MAcJUF.Wba", 5), // Password: passSensor005
+                new(Guid.NewGuid(), "sensor-006", "$2a$12$AL6MzNWBoRdS7FOF4.RRreRcNpSPXWsaIeu8id4maFMgmYKIk.89G", 6), // Password: passSensor006
+                new(Guid.NewGuid(), "sensor-007", "$2a$12$Bd49l32XKEFYqS29ZdPECOqdddMEgB0/kwg/O7pPvPSjOmEl48aiW", 7), // Password: passSensor007
             })
             {
                 await context.SensorClients.AddAsync(sensorClient);
